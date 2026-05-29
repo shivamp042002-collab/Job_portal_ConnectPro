cd7a1b4ee62ce4e3803bb4c0051d538539f79a99
shivampandey8983 <shivamshivam8409@gmail.com>
2026-05-29 17:47:48 +0530

    successfully completed deployment on aws

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn connectpro.wsgi:application --workers 3 --bind 0.0.0.0:8000
--- connectpro/settings/prod.py
+++ connectpro/settings/prod.py
@@ -1,17 +1,35 @@
 from .base import *
+import pymysql
+pymysql.install_as_MySQLdb()
 
 DEBUG = False
 
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_SECONDS = 31536000
+# Security settings
+SECURE_BROWSER_XSS_FILTER      = True
+SECURE_CONTENT_TYPE_NOSNIFF    = True
+SECURE_HSTS_SECONDS            = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_PRELOAD            = True
+SECURE_SSL_REDIRECT            = False   # Set True after HTTPS is configured
+SESSION_COOKIE_SECURE          = False   # Set True after HTTPS
+CSRF_COOKIE_SECURE             = False   # Set True after HTTPS
+X_FRAME_OPTIONS                = 'DENY'
 
+# CORS
 CORS_ALLOWED_ORIGINS = [
+    f"http://{config('EC2_IP', default='localhost')}",
     "https://yourdomain.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
+
+# Static files served by Nginx
+STATIC_ROOT = '/home/ubuntu/connectpro/staticfiles/'
+MEDIA_ROOT  = '/home/ubuntu/connectpro/media/'
+
+# Cache
+CACHES = {
+    'default': {
+        'BACKEND':  'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'connectpro-cache',
+    }
+}
